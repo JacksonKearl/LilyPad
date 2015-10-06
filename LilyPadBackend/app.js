9935ccca33d846bf3ff74ca555285245aab924ba
Jackson Kearl <jkearl@mit.edu>
2015-10-06 10:42:15 -0400

    allow cross origin

--- LilyPadBackend/app.js
+++ LilyPadBackend/app.js
@@ -24,6 +24,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.use('/', routes);
 app.use('/users', users);
 app.use('/locations', locations);
