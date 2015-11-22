e046d124555492bbc16991e2b0dd661a9f52181d
Jackson Kearl <jkearl11@gmail.com>
2015-11-21 23:41:24 -0500

    Create config.js
    for travis-ci local testing.

---
+++ config.js
@@ -0,0 +1,5 @@
+// for use by Travis-CI, running locally will require some changes.
+
+module.exports = {"url":"postgres://postgres@localhost/test",
+    'secret':"dev secret, ssh... don't tell"
+};
