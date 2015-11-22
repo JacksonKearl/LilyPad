0b89489a6b9f9ad3e05bfacb60225afb7faa6601
Jackson Kearl <jkearl11@gmail.com>
2015-11-21 23:48:59 -0500

    Update tests.js

--- LilyPadBackend/test/tests.js
+++ LilyPadBackend/test/tests.js
@@ -4,7 +4,7 @@ var request = require('supertest');
 var config = require('../config.js');
 
 describe('Routing', function() {
-    var url = 'http://lilypaddev.ddns.net:3000';
+    var url = 'http://localhost:3000';
 
     //TEST USERS
 
