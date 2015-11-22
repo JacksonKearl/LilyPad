5a722bcd08ea7608337491137153a7331b9ff11e
Jackson Kearl <jkearl11@gmail.com>
2015-11-21 23:55:49 -0500

    Delete config.js

--- LilyPadBackend/config.js
+++
@@ -1,5 +0,0 @@
-// for use by Travis-CI, running locally will require some changes.
-
-module.exports = {"url":"postgres://postgres@localhost/test",
-    'secret':"dev secret, ssh... don't tell"
-};
